a38ed05bd5d845a90b34ad82b2976973a8320a56
schwerelos <94843391+schwerelos@users.noreply.github.com>
2024-02-01 12:04:58 +0100

    Add files via upload

--- main_model.py
+++ main_model.py
@@ -33,12 +33,8 @@
 
 # IMPORTANT ！！！！
 # don't forget the neuron number
-# don't forget to change the weight
-# don't forget to add the dalay
-# the tau in main equation, pleas find the value, the previous one is arbitarly determined
-# don't forget unless refractory order
-# don't forget you have multiple group functions (eqs)
 # the current variable are all unitless
+# poisson amplitude tau_m J is 20 or 20e-3?
 
 from brian2 import *
 import matplotlib.pyplot as plt
@@ -48,19 +44,15 @@ import numpy as np
 start_scope()
 
 # define some value
-N_E = 1000      #number of excitatory and inhibitory neuron
-N_I = 250
+N_E = 100      #number of excitatory and inhibitory neuron
+N_I = 25
 
-J=0.1e-3
-g=8       #inhibitory connection factor
-J_E=J
-J_I=-(g*J)      #connection strength, fixed
 
 V_th=20e-3      # firing threshold and reset voltage of LIF
 V_r=10e-3
-t_ref=2e-3      #refractory period
+t_ref=2e-3*second     #refractory period
 
-D=1.5*ms        #trasmission delay
+D=1*ms        #trasmission delay
 
 Poisson_rate=15*kHz
 enhenced_Poisson_rate=Poisson_rate*1.4      #increase 4.% as the first picture state
@@ -72,13 +64,23 @@ beta_a=0.4 * second
 beta_d=0.4 * second
 
 # time constant for main equation and calcium concentration
-tau_m=1 * second
+tau_m=20 * ms
 tau_ca=1 * second
+tau_standard= 1 * second   # standard time constant to match the unit
 
-# zero matrix to initialize the parameters, just for the first irritation
-C_matrix= np.zeros((N_E, N_E))
-K_in=np.sum(C_matrix, axis=1)   # array[i] means K_in_i
-K_out=np.sum(C_matrix,axis=0)   # array[i] means K_out_i
+J=0.1e-3
+g=8       #inhibitory connection factor
+J_E=J
+J_I=-(g*J)      #connection strength, fixed
+J_poisson = 20*J  # set amplitude as 20 instead of 20e-3 temporary
+
+# some other constant
+weight_standard=1   #standard weight for each synapse, only C change
+
+sim_duration = 3 * ms
+total_step=int(sim_duration/defaultclock.dt)
+step_count=[0]
+
 
 
 
@@ -98,12 +100,16 @@ drou_a_abs/dt= (target_firing_rate - fai)/beta_a :1
 drou_d_abs/dt= (target_firing_rate - fai)/beta_d :1
 '''
 synapse_eqs_exc='''
-dc/dt= irri_term_pre - (c * irri_term_post)
+dc/dt= (irri_term_first - (c * irri_term_second))/tau_standard :1  (clock-driven)
+w:1
+irri_term_first:1
+irri_term_second:1
 '''
-exc_group = NeuronGroup(N_E, group_eqs_exc, threshold='v>V_th', reset='v=V_r', refractory=t_ref, method='exact')
-inh_group = NeuronGroup(N_I, group_eqs, threshold='v>V_th', reset='v=V_r', refractory=t_ref, method='exact')
+
+exc_group = NeuronGroup(N_E, model=group_eqs_exc, threshold='v>V_th', reset='v=V_r', refractory=t_ref, method='euler')
+inh_group = NeuronGroup(N_I, model=group_eqs, threshold='v>V_th', reset='v=V_r', refractory=t_ref, method='euler')
 poissoninput_group = PoissonGroup((N_E+N_I), rates=Poisson_rate)
-readout_group = NeuronGroup(1, group_eqs, threshold='v>V_th', reset='v=V_r', refractory=t_ref, method='exact')   #the first picture
+readout_group = NeuronGroup(1, model=group_eqs, threshold='v>V_th', reset='v=V_r', refractory=t_ref, method='euler')   #the first picture
 
 
 
@@ -122,48 +128,68 @@ US_marker,C1_marker,C2_marker = customized_function.assign_group(N_E,N_I)
 # input group, one to one connection, 1-2499 connect exc group 2500-3499 connect inh group
 # readout connect all US group (first picture)
 buffer = customized_function.assign_connection(N_I,N_I,0.1*N_I)
-synapse_i2i = Synapses(inh_group, inh_group, 'w : 1', on_pre='v_post += w')
+synapse_i2i = Synapses(inh_group, inh_group, 'w : 1', on_pre='v_post += J_I', method='euler', delay=D)
 # i is always the first element of butter,j the second
 synapse_i2i.connect(i=[count[0] for count in buffer], j=[count[1] for count in buffer])
-synapse_i2i.w = 'rand()'
+synapse_i2i.w = weight_standard
 
 buffer = customized_function.assign_connection(N_I,N_E,0.1*N_I)
-synapse_i2e = Synapses(inh_group, exc_group, 'w : 1', on_pre='v_post += w')
+synapse_i2e = Synapses(inh_group, exc_group, 'w : 1', on_pre='v_post += J_I', method='euler',delay=D)
 # i is always the first element of butter,j the second
 synapse_i2e.connect(i=[count[0] for count in buffer], j=[count[1] for count in buffer])
-synapse_i2e.w = 'rand()'
+synapse_i2e.w = weight_standard
 
 buffer = customized_function.assign_connection(N_E,N_I,0.1*N_E)
-synapse_e2i = Synapses(exc_group, inh_group, 'w : 1', on_pre='v_post += w')
+synapse_e2i = Synapses(exc_group, inh_group, 'w : 1', on_pre='v_post += J_E', method='euler',delay=D)
 # i is always the first element of butter,j the second
 synapse_e2i.connect(i=[count[0] for count in buffer], j=[count[1] for count in buffer])
-synapse_e2i.w = 'rand()'
+synapse_e2i.w = weight_standard
 
 # bind poisson group 1-2500 to exc 2501-3500 to ihn, connection is one to one
-synapse_p2e = Synapses(poissoninput_group, exc_group, 'w : 1', on_pre='v_post += w')
+synapse_p2e = Synapses(poissoninput_group, exc_group, 'w : 1', on_pre='v_post += J_poisson', method='euler',delay=D)
 synapse_p2e.connect(i=list(range(0,N_E,1)), j=list(range(0,N_E,1)))    #i and j is the same, from 0 to N_E-1
-synapse_p2e.w = 'rand()'
+synapse_p2e.w = weight_standard
 
-synapse_p2i = Synapses(poissoninput_group, inh_group, 'w : 1', on_pre='v_post += w')
+synapse_p2i = Synapses(poissoninput_group, inh_group, 'w : 1', on_pre='v_post += J_poisson', method='euler',delay=D)
 synapse_p2i.connect(i=list(range(N_E,N_E+N_I,1)), j=list(range(0,N_I,1)))   #2500-3499 of poisson connect to 0-1499 of inh
-synapse_p2i.w = 'rand()'
+synapse_p2i.w = weight_standard
 
 # bind all exc to readout group
-synapse_e2r = Synapses(exc_group, readout_group, 'w : 1', on_pre='v_post += w')
+synapse_e2r = Synapses(exc_group, readout_group, 'w : 1', on_pre='v_post += J_E', method='euler',delay=D)
 synapse_e2r.connect(i=list(range(0,N_E,1)), j=[0])   # all exc (0-2499) connect to readout[0]
-synapse_e2r.w = 'rand()'
+synapse_e2r.w = weight_standard
 
 # bind all exc to exc but with zero initial weight
-# because the initial C is a zero matrix
-synapse_e2e = Synapses(exc_group, exc_group, 'w : 1', on_pre='v_post += w')
+synapse_e2e = Synapses(exc_group, exc_group, model=synapse_eqs_exc, on_pre='v_post += J_E*c', method='euler',delay=D)
 synapse_e2e.connect(condition='i!=j')   # all econnect to all exc
-synapse_e2e.w = 'rand()'
+synapse_e2e.w = weight_standard
+
+
 
 #customized_function.check_synapseconnection(synapse_e2e)
 
-network = Network(exc_group , inh_group , poissoninput_group , readout_group,
-synapse_i2i , synapse_i2e , synapse_e2i , synapse_p2i , synapse_p2e , synapse_e2r
-)
+# network = Network(exc_group , inh_group , poissoninput_group , readout_group,
+# synapse_i2i , synapse_i2e , synapse_e2i , synapse_p2i , synapse_p2e , synapse_e2r
+# )
+
+# zero matrix to initialize the parameters, just for the first irritation
+C_matrix= np.ones((N_E, N_E))
+K_in=np.sum(C_matrix, axis=1)   # array[i] means K_in_i  +1 helps to avoid the /0 error during the first run
+K_out=np.sum(C_matrix,axis=0)   # array[i] means K_out_i
+
+synapse_e2e.c=1   # give one connections for each in the initial step to prevent the /0 error
+
+synapse_e2e.irri_term_first=0
+synapse_e2e.irri_term_second=0
+
+# some monitors
+Vol_mon=StateMonitor(exc_group,'v',record=[1,2,3])
+abs_mon=StateMonitor(exc_group,'rou_a_abs',record=[1,2,3])
+fai_mon=StateMonitor(exc_group,'fai',record=[1,2,3])
+c_mon=StateMonitor(synapse_e2e,'c',record=[1,2,3])
+poi_mon=SpikeMonitor(poissoninput_group)
+
+
 
 @network_operation(when='before_synapses')
 def my_network_operation():
@@ -173,26 +199,32 @@ def my_network_operation():
     # convert synaptic variable c into matrix form
     # don't need to be converted back because c will irritate in ODE independently
     # note that the physical meaning of C[i][j] is i from j, but not i to j
-    C_matrix = customized_function.synapsevar_matrix_generator(synapse_e2e, synapse_e2e.c)
+    C_matrix = customized_function.synapsevar_to_matrix(synapse_e2e, synapse_e2e.c)
 
     # calculate Kin and Kout
     K_in = np.sum(C_matrix, axis=1)  # array[i] means K_in_i
     K_out = np.sum(C_matrix, axis=0)  # array[i] means K_out_i
 
+
     # from abs to real free element number, abs should be prepared during the step in group variablees
-    rou_a_plus = np.clip(rou_a_abs, 0, np.inf)   # free axon surplus, larger than or equal to zero
-    rou_a_minus = np.clip(rou_a_abs, -np.inf, 0)   # free axon deficit, smaller than or equal to zero
-    rou_d_plus = np.clip(rou_d_abs, 0, np.inf)   # free dendritic surplus, larger than or equal to zero
-    rou_d_minus = np.clip(rou_d_abs, -np.inf, 0)  # free dendritic deficit, smaller than or equal to zero
+    rou_a_plus = np.clip(exc_group.rou_a_abs, 0, np.inf)   # free axon surplus, larger than or equal to zero
+    rou_a_minus = np.clip(exc_group.rou_a_abs, -np.inf, 0)   # free axon deficit, smaller than or equal to zero
+    rou_d_plus = np.clip(exc_group.rou_d_abs, 0, np.inf)   # free dendritic surplus, larger than or equal to zero
+    rou_d_minus = np.clip(exc_group.rou_d_abs, -np.inf, 0)  # free dendritic deficit, smaller than or equal to zero
 
     # prepare for correction
     rows,cols=C_matrix.shape
     buffer_a = np.zeros(rows)
     buffer_d = np.zeros(rows)
 
-    for count in range(rows)
-        buffer_a += (rou_d_minus[count]/K_in[count]) .* (C_matrix[count][:]) # correction term  ad sequence is verified
-        buffer_d += (rou_a_minus[count]/K_out[count]) .* (C_matrix[:][count])
+    for count in range(rows):
+        buffer_a += (rou_d_minus[count]/K_in[count]) * (C_matrix[count][:]) # correction term  ad sequence is verified
+        buffer_d += (rou_a_minus[count]/K_out[count]) * (C_matrix[:][count])
+
+    # for the first round, Kin Kout is 0, induce nan error
+    # repalce all na with 0 to prevent the initialization bug
+    buffer_a[isnan(buffer_a)]=0
+    buffer_d[isnan(buffer_d)]=0
 
     # generate correction term  these are array and final rou
     rou_a_plus_corrected = rou_a_plus + buffer_a
@@ -200,18 +232,34 @@ def my_network_operation():
     rou = max(sum(rou_a_plus_corrected),sum(rou_d_plus_corrected))   # should be a number
 
     # prepare irritation matrix for C
-    irri_matrix_pre = np.zeros.like(C_matrix.shape)   #irritation term in synaptic equation, to be converted
-    irri_matrix_post = np.zeros.like(C_matrix.shape)   #second parameter in the synaptic ODE
-    irri_matrix_pre[i][j] = rou_d_plus[i]*rou_a_plus[j]/rou   #fill in the data based on the ODE
-    irri_matrix_post[i][j] = (rou_d_minus[i] / K_in[i])+(rou_a_minus[j] / K_out[j])
+    irri_matrix_first = np.zeros(C_matrix.shape)   #irritation term in synaptic equation, to be converted
+    irri_matrix_second = np.zeros(C_matrix.shape)   #second parameter in the synaptic ODE
+
+    # fill the irritation matrix for C, only the elements on the synapse_e2e list are involved
+    for i, j in zip(synapse_e2e.i, synapse_e2e.j):
+        irri_matrix_first[i][j] = rou_d_plus[i]*rou_a_plus[j]/rou   #fill in the data based on the ODE
+        irri_matrix_second[i][j] = (rou_d_minus[i] / K_in[i])+(rou_a_minus[j] / K_out[j])
+
+    # for the first round, Kin Kout is 0, induce nan error
+    # repalce all na with 0 to prevent the initialization bug
+    irri_matrix_first[isnan(irri_matrix_first)]=0
+    irri_matrix_second[isnan(irri_matrix_second)]=0
 
     # convert that into standard brain 2 form for dc/dt irritation
     # irripre and irripost should be in shape [N,1], in which N is the connection number with sequenced element
     # transpose inside the customized function make sure that [i][j] represents i from j after being transposed
-    irri_term_pre = customized_function.matrix_to_synapsevar(synapse_e2e, irri_matrix_pre)
-    irri_term_post = customized_function.matrix_to_synapsevar(synapse_e2e, irri_matrix_post)
+    irri_term_first = customized_function.matrix_to_synapsevar(synapse_e2e, irri_matrix_first)
+    irri_term_second = customized_function.matrix_to_synapsevar(synapse_e2e, irri_matrix_second)
 
+    #print("progress bar" )
+    step_count[0]+=1
+    print(step_count, ' of ', total_step,' has been finished')
 
-sim_duration = 10 * ms
-network.run=(sim_duration)
+    plot(poi_mon.t / ms, poi_mon.i, '.k')
+
+run(sim_duration)
+pass
+
+#network.run=(sim_duration)
+
 
